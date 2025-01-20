6868f45c5bf72d317c9b9a85c24da3e85bf7965a
SandraPautonnier <sandrapautonnier@gmail.com>
2025-01-20 15:37:57 +0100

    update userSlice

--- Frontend/src/redux/userSlice.jsx
+++ Frontend/src/redux/userSlice.jsx
@@ -1,37 +1,3 @@
-/*import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
-  firstName: localStorage.getItem("{firstName}") || "",
-  lastName: localStorage.getItem("{lastName}") || "",
-  userName: localStorage.getItem("{userName}") || "",
-};
-
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    userProfile: (state, action) => {
-      const { userName, firstName, lastName } = action.payload;
-
-      state.firstName = firstName;
-      state.lastName = lastName;
-      state.userName = userName;
-
-      localStorage.setItem("firstName", firstName);
-      localStorage.setItem("lastName", lastName);
-      localStorage.setItem("userName", userName);
-    },
-    updateUserName: (state, action) => {
-      const { userName } = action.payload;
-      state.userName = userName;
-      localStorage.setItem("userName", userName);
-    },
-  },
-});
-
-export const { userProfile, updateUserName } = userSlice.actions;
-export default userSlice.reducer; */
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {

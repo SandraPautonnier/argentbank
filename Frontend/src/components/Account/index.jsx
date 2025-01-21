586675081e2ff3771141aff92480010349ee7300
SandraPautonnier <sandrapautonnier@gmail.com>
2025-01-21 11:23:57 +0100

    add proptypes account

--- Frontend/src/components/Account/index.jsx
+++ Frontend/src/components/Account/index.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Account = () => {
+const Account = ({ title, amount, description }) => {
   return (
-    <div>
-        <section className="account"> 
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-    </div>
-  )
-}
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
+  );
+};
 
-export default Account
+Account.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Account;
--- Frontend/src/pages/User/index.jsx
+++ Frontend/src/pages/User/index.jsx
@@ -1,32 +1,54 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-import { loginSuccess } from '../../redux/userSlice';
-import EditUser from '../../components/EditUser'
-import NavBar from '../../components/NavBar'
-import Footer from '../../components/Footer'
-import Account from '../../components/Account';
-
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { loginSuccess } from "../../redux/userSlice";
+import EditUser from "../../components/EditUser";
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+import Account from "../../components/Account";
 
 const User = () => {
+  const accounts = [
+    {
+      title: "Argent Bank Checking (x8349)",
+      amount: "$2,082.79",
+      description: "Available Balance",
+    },
+    {
+      title: "Argent Bank Checking (x8349)",
+      amount: "$2,082.79",
+      description: "Available Balance",
+    },
+    {
+      title: "Argent Bank Checking (x8349)",
+      amount: "$2,082.79",
+      description: "Available Balance",
+    },
+  ];
 
   if (!loginSuccess) {
-    return <Navigate to='/login' />
+    return <Navigate to="/login" />;
   }
 
   return (
-    <div className='content-body'>
+    <div className="content-body">
       <NavBar />
       <main className="main bg-dark">
         <EditUser />
         <h2 className="sr-only">Accounts</h2>
-        <Account />
-        <Account />
-        <Account />
+        <div className="accounts">
+          {accounts.map((account, index) => (
+            <Account
+              key={index}
+              title={account.title}
+              amount={account.amount}
+              description={account.description}
+            />
+          ))}
+        </div>
       </main>
       <Footer />
     </div>
-    
-  )
-}
+  );
+};
 
-export default User
+export default User;

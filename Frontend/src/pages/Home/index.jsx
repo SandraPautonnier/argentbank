453f75105c8b12c4816ddb5ee4596e49192583d8
SandraPautonnier <sandrapautonnier@gmail.com>
2025-01-21 11:00:54 +0100

    add proptypes for feature item

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import NavBar from '../../components/NavBar'
 import Footer from '../../components/Footer'
+import FeatureItem from '../../components/FeatureItem';
 import IconChat from "../../assets/img/icon-chat.png";
 import IconMoney from "../../assets/img/icon-money.png";
 import IconSecurity from "../../assets/img/icon-security.png"
 
 
 const Home = () => {
+
+  const features = [
+    {
+      icon: IconChat,
+      title: 'You are our #1 priority',
+      description:
+        'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    },
+    {
+      icon: IconMoney,
+      title: 'More savings means higher rates',
+      description:
+        'The more you save with us, the higher your interest rate will be!',
+    },
+    {
+      icon: IconSecurity,
+      title: 'Security you can trust',
+      description:
+        'We use top of the line encryption to make sure your data and money is always safe.',
+    },
+  ];
+
   return (
     <div className='content-body'>
     <NavBar />    
@@ -22,36 +45,15 @@ const Home = () => {
       </div>
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        <div className="feature-item">
-          <img src={IconChat} alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={IconMoney}
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">More savings means higher rates</h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={IconSecurity}
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
+        <div className="features">
+          {features.map((feature, index) => (
+            <FeatureItem
+              key={index}
+              icon={feature.icon}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
         </div>
       </section>
     </main>
